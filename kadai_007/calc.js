2e3578e653b125b981a71cc2f0da5702ee95d5f8
maam23 <maamii0007@gmail.com>
2024-08-20 21:04:46 +0900

    kadai_007アップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+let num = 15
+
+if (num === 3) {
+    console.log('3の倍数です')
+}
+
+if (num === 5) {
+    console.log('5の倍数です')
+}
+
+if (num === 3 && num === 5) {
+    console.log('3と5の倍数です')
+}
+
+else {
+    console.log(num);
+
+}
